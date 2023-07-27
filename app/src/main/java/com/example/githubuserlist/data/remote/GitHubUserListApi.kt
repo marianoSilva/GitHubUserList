f2710febd030b6021b73a58f11859f82a28ec671
masv <masv@gft.com>
2023-07-27 17:34:02 -0300

    Remove headerr

--- app/src/main/java/com/example/githubuserlist/data/remote/GitHubUserListApi.kt
+++ app/src/main/java/com/example/githubuserlist/data/remote/GitHubUserListApi.kt
@@ -4,15 +4,12 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitHubUserListApi {
-    @Headers("Authorization: Bearer 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")
     @GET("users")
     suspend fun getUserData() : UserDtoList
 
-    @Headers("Authorization: Bearer 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")
     @GET("users/{url}")
     suspend fun getDetailData(@Path(value = "url", encoded = true) url : String) : UserDetailDto
 
-    @Headers("Authorization: Bearer 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")
     @GET("users/{url}/repos")
     suspend fun getRepositoryData(@Path(value = "url", encoded = true) url : String) : RepositoryDtoList
 }
