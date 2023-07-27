9ea9fa451a754fb9eb59eb347751f821377d1133
masv <masv@gft.com>
2023-07-26 23:26:04 -0300

    Created Detail Presentation

--- app/src/main/java/com/example/githubuserlist/data/remote/GitHubUserListApi.kt
+++ app/src/main/java/com/example/githubuserlist/data/remote/GitHubUserListApi.kt
@@ -5,15 +5,15 @@ import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface GitHubUserListApi {
-    @Headers("Authorization: Bearer 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")
+    @Headers("Authorization: Bearer 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")
     @GET("users")
     suspend fun getUserData() : UserDtoList
 
-    @Headers("Authorization: Bearer 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")
+    @Headers("Authorization: Bearer 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")
     @GET("users/{url}")
     suspend fun getDetailData(@Path(value = "url", encoded = true) url : String) : UserDetailDto
 
-    @Headers("Authorization: Bearer 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")
+    @Headers("Authorization: Bearer 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")
     @GET("users/{url}/repos")
     suspend fun getRepositoryData(@Path(value = "url", encoded = true) url : String) : RepositoryDtoList
 }
--- app/src/main/java/com/example/githubuserlist/presentation/DetailScreen.kt
+++ app/src/main/java/com/example/githubuserlist/presentation/DetailScreen.kt
@@ -1,24 +1,61 @@
 package com.example.githubuserlist.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.githubuserlist.presentation.detail.DetailViewModel
+import com.example.githubuserlist.presentation.detail.ReposListView
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen(login: String){
     val viewModel: DetailViewModel = hiltViewModel()
     LaunchedEffect(Unit){
         viewModel.loadDetail(login)
     }
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()){
-        Text(text = "Hello, ${viewModel.state.detailData?.name}.")
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text("Detalhes",fontWeight = FontWeight.Bold)
+            }, Modifier.background(color = Color.LightGray))
+        }
+    ) { contentPadding ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(contentPadding)){
+            Column(Modifier.fillMaxSize().padding(8.dp),) {
+                Text(text = "Nome: ${viewModel.state.detailData?.name}.",
+                    modifier = Modifier.padding(top = 10.dp))
+                Text(text = "Email: ${viewModel.state.detailData?.email}.",
+                modifier = Modifier.padding(top = 10.dp))
+                Row(modifier = Modifier
+                    .padding(top = 10.dp)
+                    .fillMaxWidth(),
+                    horizontalArrangement  =  Arrangement.SpaceBetween) {
+                    Text(text = "Seguidores: ${viewModel.state.detailData?.followers}.")
+                    Text(text = "Local: ${viewModel.state.detailData?.location}.")
+                }
+                Text(text = "Repositorios(${viewModel.state.detailData?.publicRepos}).",
+                    modifier = Modifier.padding(top = 40.dp))
+                ReposListView(state = viewModel.state)
+            }
+        }
+
     }
 }
